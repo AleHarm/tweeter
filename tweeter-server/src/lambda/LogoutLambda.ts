c22b47682ed8ebbca98c7043e2773d1452795eea
AlexHarmon2282 <aharmon@moxiepestcontrol.com>
2024-03-20 05:30:29 -0600

    Make logout lambda async to show info toast

--- tweeter-server/src/lambda/LogoutLambda.ts
+++ tweeter-server/src/lambda/LogoutLambda.ts
@@ -1,7 +1,7 @@
 import { LogoutRequest } from "tweeter-shared/dist/model/net/Requests/LogoutRequest";
 import { UserService } from "../model/service/UserService";
 
-export const handler = (event: LogoutRequest): void => {
+export const handler = async (event: LogoutRequest) => {
 
-  new UserService().logout(event.authToken);
+  await new UserService().logout(event.authToken);
 };
