ce0f55ecfdbf027ccc4a5a08d53e128cd1f1997a
AlexHarmon2282 <aharmon@moxiepestcontrol.com>
2024-03-25 11:45:46 -0600

    Milestone 3 complete

--- tweeter-web/test/serverFacade/ServerFacade.test.ts
+++ tweeter-web/test/serverFacade/ServerFacade.test.ts
@@ -1,11 +1,31 @@
 import { ServerFacade } from "../../src/net/ServerFacade";
-
-
+import { GetFollowersCountRequest, GetUserRequest, RegisterRequest } from "tweeter-shared/src/model/net/Request";
+import { AuthToken } from "tweeter-shared/src/model/domain/AuthToken";
+import { User } from "tweeter-shared/src/model/domain/User";
+import "isomorphic-fetch";
 
 describe("ServerFacade", () => {
+  let serverFacade: ServerFacade;
 
-const serverFacade = new ServerFacade();
+  beforeEach(() => {
+    serverFacade = new ServerFacade();
+  });
 
+  it("registers a user", async () => {
+    const request = new RegisterRequest("bill", "Stevens", "@BillyBoi", "a22ffododod", "");
+    let value = await serverFacade.register(request);
+    expect(value).not.toBeNull();
+  });
 
+  it("gets users", async () => {
+    const request = new GetUserRequest("@allen", new AuthToken("a456699", Date.now()));
+    let value = await serverFacade.getUser(request);
+    expect(value).not.toBeNull();
+  });
 
+  it("gets a number of followers", async () => {
+    const request = new GetFollowersCountRequest(new AuthToken("a456699", Date.now()), new User("bill", "Stevens", "@BillyBoi", ""));
+    let value = await serverFacade.getFollowersCount(request);
+    expect(value).not.toBeNull();
+  });
 });
